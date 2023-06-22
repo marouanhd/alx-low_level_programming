f1e25a6302ed831ed9aa0dfa95a899ebc932d3dd
marouanhd <maroan.ma1999@gmail.com>
2023-06-22 15:45:16 -0700

    100-prime

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,28 +1,29 @@
 #include <stdio.h>
-#include <math.h>
+
 /**
- * main - finds the largest prime
- * Description: Uses headers to link and a nested loops to achieve goal
- * Return: 0 if no errors
+ * main - prints the largest prime factor of the number 612852475143
+ *
+ * Description:
+ *		- A factor is a number that can be evenly divided into another number.
+ *			For example, the factors of 6 are 1, 2, 3, and 6.
+ *		- A prime factor is a factor that is also a prime number, which means
+ *			it can only be divided by 1 and itself. For example, the prime
+ *			factors of 6 are 2 and 3.
+ *
+ * Return: 0
  */
-
-
 int main(void)
 {
-	unsigned long i = 2;
-	unsigned long largest = 0;
-	unsigned long n = 612852475143;
+	long int n = 612852475143;
+	long int i;
 
-	while (n > i)
+	for (i = 2; i < n; i++)
 	{
 		while (n % i == 0)
-		{
-			if (i > largest)
-				largest = i;
-			n = n / i;
-		}
-		i++;
+			n /= i;
 	}
-	printf("%lu\n", largest);
+
+	printf("%ld\n", n);
+
 	return (0);
 }
